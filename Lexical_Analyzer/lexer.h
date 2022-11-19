87df59766f5a095d8435aa72dd4306063f453b8c
dbouros <74077308+dbouros@users.noreply.github.com>
2022-11-19 12:48:41 +0200

    Add files via upload

---
+++ Lexical_Analyzer/lexer.h
@@ -0,0 +1,68 @@
+// Tokens
+
+#define T_EOF 0 
+
+// Keywords
+#define T_PROGRAM 1
+#define T_CONST 2
+#define T_TYPE 3
+#define T_ARRAY 4
+#define T_LIST 5
+#define T_SET 6
+#define T_OF 7
+#define T_RECORD 8
+#define T_VAR 9
+#define T_FUNCTION 10
+#define T_PROCEDURE 11
+#define T_INTEGER 12
+#define T_REAL 13
+#define T_BOOLEAN 14
+#define T_CHAR 15
+#define T_FORWARD 16
+#define T_LENGTH 17
+#define T_NEW 18
+#define T_BEGIN 19
+#define T_END 20
+#define T_IF 21
+#define T_THEN 22
+#define T_ELSE 23
+#define T_WHILE 24
+#define T_DO 25
+#define T_CASE 26
+#define T_OTHERWISE 27
+#define T_FOR 28
+#define T_TO 29
+#define T_DOWNTO 30
+#define T_WITH 31
+#define T_READ 32
+#define T_WRITE 33
+
+// Identifier
+#define T_ID
+
+// Constants
+#define T_ICONST
+#define T_RCONST
+#define T_BCONST
+#define T_CCONST
+
+// Operators
+#define T_RELOP
+#define T_ADDOP
+#define T_OROP
+#define T_MULDIVANDOP
+#define T_NOTOP
+#define T_INOP
+
+// Other Lexical Tokens
+#define T_LPAREN
+#define T_RPAREN
+#define T_SEMI
+#define T_DOT
+#define T_COMMA
+#define T_EQU
+#define T_COLON
+#define T_LBRACK
+#define T_RBRACK
+#define T_ASSIGN
+#define T_DOTDOT
