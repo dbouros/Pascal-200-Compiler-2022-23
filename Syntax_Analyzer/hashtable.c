7a48955bdc14971065264aa3596711358ea8d7fb
dbouros <jimmisp92@gmail.com>
2023-01-23 15:50:20 +0200

    Changing Hashtable program

--- Syntax_Analyzer/hashtable.c
+++ Syntax_Analyzer/hashtable.c
@@ -10,19 +10,3 @@ struct h_Node{
 
     struct h_Node *next;
 };
-
-typedef struct h_Table{
-
-    h_Size size;
-    struct h_Node **nodes;
-    h_Size (*hashfunc)(const char *);
-    // Pointer *hashfunc == function pointer, with size (h_Size) and type (const char *)
-    
-}HASHTABLE;
-
-// Hash table's functions
-HASHTABLE *h_Table_Create(h_Size size, h_Size (*hashfunc)(const char *));
-void h_Table_Delete(HASHTABLE *hashtable);
-int h_Table_insert(HASHTABLE *hashtable, const char *key, void *data, int scope);
-int h_Table_remove(HASHTABLE *hashtable, const char *key, int scope);
-void *h_Table_Get(HASHTABLE *hashtable, int scope);
